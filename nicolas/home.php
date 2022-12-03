b3dc3f1b383e036274fe5815d8c3aaae199b3658
Nicolas Gabriel da Silva <72479058+nicolasdsgabriel@users.noreply.github.com>
2022-12-02 21:59:51 -0300

    Add files via upload

---
+++ nicolas/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slash</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <header class="home-cabeçalho">
+        <img class="home-cabeçalho_img" src="img/SLASHCRIT!.png" alt="Logo do site">
+        <nav class="cabeçalho-menu">
+            <a class="cabeçalho-cadastro" href="cadastro.php">Cadastre-se</a>
+            <a class="cabeçalho-login" href="login.php">Login</a>
+        </nav>
+    </header>
+    <main class="conteudo">
+        <section class="conteudo-principal">
+            <div class="conteudo-principal-texto">
+                <h2 class="conteudo-principal-subtitulo">Contribua para uma comunidade apaixonada</h2>
+                <button class="conteudo-principal-botao">Vamos!</button>
+            </div>
+            <img class="conteudo-principal-img" src="img/BEM VINDO!.png" alt="Imagem de boas vindas">
+        </section>
+        <section class="conteudo-secundario">
+            <h3 class="conteudo-secundario-titulo">O que vem a seguir ?</h3>
+            <p class="conteudo-secundario-texto">Testo text</p>
+        </section>
+    </main>
+    <footer class="home-rodape">
+        <img class="home-rodape-img" src="img/SLASHCRIT!_TEXT.png" alt="Nome do site">
+    </footer>
+</body>
+</html>
